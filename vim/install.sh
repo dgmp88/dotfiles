0eac05f85c3e28ba490ad4e0ea8a34fde30b442c
dgmp88 <dgmprichard@gmail.com>
2016-03-11 11:33:08 +0000

    All tha mods

---
+++ vim/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Vundle
+#
+#
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,5 @@
 alias reload!='. ~/.zshrc'
+
+dev=/Users/george/dev
+data=/Users/george/data
+rps=/Users/george/dev/random-public-site/randomsite
