bc488db21ce519924238de2ae400e9bb400e2340
George Prichard <dgmprichard@gmail.com>
2018-07-03 16:03:27 +0100

    Put OS installation stuff in one place

--- os/install.sh
+++ os/install.sh
@@ -4,10 +4,11 @@ if test "$(uname)" = "Darwin"
 then
 	echo "› sudo softwareupdate -i -a"
 	sudo softwareupdate -i -a
+  	brew bundle
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
 	sudo apt-get update
-	sudo apt-get install zsh wget
+	sudo apt-get install -y zsh wget git
 fi
 
 
