d919b73c9b9e2226112e874e100845929d95b403
George Prichard <dgmprichard@gmail.com>
2018-09-03 14:29:40 +0100

    Vimrc -> neovim

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+ln -s "$HOME/.dotfiles/vim/vimrc.symlink" "$HOME/.config/nvim/init.vim"
