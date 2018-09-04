2cfc4be95c5be8367c5788ff8c45861c35a3a483
dgmp88 <dgmprichard@gmail.com>
2018-09-04 14:49:53 +0000

    Minor changes to make things automated-er

--- vim/install.sh
+++ vim/install.sh
@@ -3,4 +3,5 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s "$HOME/.dotfiles/vim/vimrc.symlink" "$HOME/.config/nvim/init.vim"
+mkdir -p ~/.config/nvim
+ln -s -b "$HOME/.dotfiles/vim/vimrc.symlink" "$HOME/.config/nvim/init.vim"
